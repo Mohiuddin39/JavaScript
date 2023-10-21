00eb65a10ebc07312d643c901e56d05143b1fc1c
Mohiuddin39 <mohiuddin169190@gmail.com>
2023-10-21 18:10:12 +0500

    01-Variuble learn

---
+++ 01_Basic/01-variables.js
@@ -0,0 +1,20 @@
+const a = 12345;
+let name = "Mohiuddin";
+var email = "g@google.com";
+place = "Pindi Abdul Rahaman"; // Highly Not Prefered
+
+let alpha; // undefined
+// a = 45;
+// console.log("🚀 ~ file: 01-variables.js:11 ~ a:", a); //not allowed
+name = "ghulam";
+console.log("🚀 ~ file: 01-variables.js:13 ~ name:", name);
+email = "ne@google.com";
+console.log("🚀 ~ file: 01-variables.js:15 ~ email:", email);
+place = "Lahore";
+console.log("🚀 ~ file: 01-variables.js:17 ~ place:", place);
+
+/*
+Not to use var in JavaScript
+Because of issue in block and functional Scoape
+
+*/
