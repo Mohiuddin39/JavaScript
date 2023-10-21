2f652cf5aa7872d07cba9db0ca0579e4d2a999ea
Mohiuddin39 <mohiuddin169190@gmail.com>
2023-10-21 18:37:42 +0500

    02-dataTypes

---
+++ 01_Basic/02-dataTypes.js
@@ -0,0 +1,30 @@
+let anme = "Mohiuddin"; // string
+let age = 18; // number
+let isComming = true; // boolean
+
+let abc = undefined; // undefined
+console.log("🚀 ~ file: 02-dataTypes.js:6 ~ abc:", typeof abc);
+let nbs = null; // object
+console.log("🚀 ~ file: 02-dataTypes.js:7 ~ nbs:", typeof nbs);
+
+/*
+//Premative DataTypes OR JavaScript DataTypes
+
+1 = Number
+2 = Bigint
+3 = Boolean
+4 = string
+5 = null // standAlone value
+6 = undefined  
+7 = symbol
+8 = Object
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date
+
+
+*/
