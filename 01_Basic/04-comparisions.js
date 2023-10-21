de1e5cb400c2d8a320c3f89417aa553600f79ff3
Mohiuddin39 <mohiuddin169190@gmail.com>
2023-10-21 23:31:53 +0500

    04-Comparisions

---
+++ 01_Basic/04-comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 >= 2);
+// console.log(2 != 2);
+
+//Now we should avoid these type of conversions because these type of conversion gernate unperdictuable Results
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined > 0);
+
+// ===  Strick Check
+console.log("2" === 2);
